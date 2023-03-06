e7d81c1a6e4e5fde1a5d25d023010050bebafaa6
mardibak <mardibaki@gmail.com>
2023-03-06 23:09:45 +0330

    add todo entity

---
+++ OLS.Domain.Entities/Entities/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using AppFramework.Domain;
+using System.Xml.Linq;
+
+namespace MLS.Domain.Entities
+{
+    public class TodoItem : BaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Note { get; set; }
+        public long ListId { get; set; }
+        public TodoList? ListName { get; set; }
+
+        //public PriorityLevel Priority { get; set; }
+        //public DateTime? Reminder { get; set; }
+
+        public void Edit(string? title, string? note, long listId)
+        {
+            Title = title;
+
+            Note = note;
+
+            ListId = listId;
+        }
+    }
+}
---
+++ OLS.Domain.Entities/Entities/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using AppFramework.Domain;
+
+namespace MLS.Domain.Entities
+{
+    public class TodoList : BaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string ListImage { get; set; }
+        public List<TodoItem>? Items { get; set; }
+
+
+    }
+}
