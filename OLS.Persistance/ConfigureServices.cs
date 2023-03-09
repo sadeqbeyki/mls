0e60521085bdb9085c4f5c919222947804f5c3a6
sadeqbeyki <mardibaki@gmail.com>
2023-03-09 23:26:12 +0330

    configure service implemented

---
+++ OLS.Persistance/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MLS.Application.TodoItems;
+using MLS.Application.TodoLists;
+using MLS.Infrastructure.Repositories;
+using MLS.Infrastructure.Persistance;
+using MLS.Infrastructure.Persistance.Repositories;
+
+namespace OLS.Persistance;
+
+public class ConfigureServices
+{
+    public static void Configure(IServiceCollection services, string connectionString)
+    {
+        services.AddScoped<ITodoListRepository, TodoListRepository>();
+        services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+
+        services.AddDbContext<TodoContext>(x => x.UseSqlServer(connectionString));
+    }
+}
