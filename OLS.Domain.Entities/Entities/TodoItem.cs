466bc82b5da8f5bf3faed2137f46b3870423b420
sadeqbeyki <mardibaki@gmail.com>
2023-03-20 22:42:14 +0330

    todoitem edited 3

--- OLS.Domain.Entities/Entities/TodoItem.cs
+++ OLS.Domain.Entities/Entities/TodoItem.cs
@@ -8,10 +8,7 @@ public class TodoItem : BaseEntity
     public string? Title { get; set; }
     public string? Note { get; set; }
     public long ListId { get; set; }
-    public TodoList? ListName { get; set; }
-
-    //public PriorityLevel Priority { get; set; }
-    //public DateTime? Reminder { get; set; }
+ 
 
     public void Edit(string? title, string? note, long listId)
     {
