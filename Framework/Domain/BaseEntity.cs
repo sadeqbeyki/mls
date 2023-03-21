452db218fb647f7d4fb952b3659ee708d6c6b7ab
sadeqbeyki <mardibaki@gmail.com>
2023-03-21 22:33:47 +0330

    base entity updated

--- Framework/Domain/BaseEntity.cs
+++ Framework/Domain/BaseEntity.cs
@@ -3,7 +3,6 @@
     public class BaseEntity
     {
         public long Id { get; set; }
-        //public long Id { get; private set; }
         public DateTime CreationDate { get; private set; }
         public BaseEntity()
         {
