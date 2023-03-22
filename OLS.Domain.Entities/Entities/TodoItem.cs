4a74e2722bd05ad7438df4cb93d70872768f3e76
sadeqbeyki <mardibaki@gmail.com>
2023-03-22 09:56:13 +0430

    update framework and todoitem 4

--- OLS.Domain.Entities/Entities/TodoItem.cs
+++ OLS.Domain.Entities/Entities/TodoItem.cs
@@ -8,6 +8,7 @@ public class TodoItem : BaseEntity
     public string? Title { get; set; }
     public string? Note { get; set; }
     public long ListId { get; set; }
+    public TodoList ListName { get; set; }
  
 
     public void Edit(string? title, string? note, long listId)
